f5c463550036ca897a60f173ac4b6b679f840ff6
yongama <yongamafayo@gmail.com>
2020-07-07 13:26:37 +0200

    first commit

--- RadioBill.test.js/RadioBill.test.js
+++ RadioBill.test.js/RadioBill.test.js
@@ -1,53 +1,158 @@
-describe("CallTotal", function () {
+describe(radioBill, function(){
+    
+    it("should charge R2.75 for each call made", function(){
+        var radioButton = RadioBill();
+        radioButton.callIncrement();
+        assert.equal(2.75,radioButton.callMade())
 
-    it("should be able to set call total", function () {
-        let settingsTest = RadioBill();
+    })
+    it("should update calls total to R22 for 8 calls made",function(){
+        var radioButton = RadioBill();
+        radioButton.callIncrement();
+        radioButton.callIncrement();
+        radioButton.callIncrement();
+        radioButton.callIncrement();
+        radioButton.callIncrement();
+        radioButton.callIncrement();
+        radioButton.callIncrement();    
+        radioButton.callIncrement();
+        
+        assert.equal(22,radioButton.callMade())
+        
+    })
 
-        settingsTest.setCallTotal("call");
-        assert.equal(2.75, settingsTest.getCallTotal());
-
-    });
-
-    it("should be able to set 2 calls total", function () {
-        let settingsTest = RadioBill();
-
-        settingsTest.setCallTotal("call");
-        settingsTest.setCallTotal("call");
-        assert.equal(5.50, settingsTest.getCallTotal());
-
-    });
-
-
-});
-
-describe("SMS Total", function () {
-
-    it("should be able to calculate smsTotal", function () {
-        let settingsTest = RadioBill();
-
-        settingsTest.setCallTotal("sms");
-        assert.equal(0.75, settingsTest.getSMSTotal());
+    it("should charge R0.75 for each sms made", function(){
+     var radioButton = RadioBill();
+     radioButton.smsIncrement();
+     assert.equal(0.75, radioButton.smsIncrement())
+ 
+    })
+    it("should update sms total to R6 for 8 sms made",function(){
+        var radioButton = RadioBill();
+        radioButton.smsIncrement();
+        radioButton.smsIncrement();
+        radioButton.smsIncrement();
+        radioButton.smsIncrement();
+        radioButton.smsIncrement();
+        radioButton.smsIncrement();
+        radioButton.smsIncrement();
+        radioButton.smsIncrement();
+        
+        assert.equal(8,radioButton.smsMade())
 
 
-    }); 
+    })
+    it("it should be able to add call and sms made", function(){
+        var radioButton = RadioBill();
+        radioButton.smsIncrement();
+        radioButton.callIncrement();
+        assert.equal(3.5,radioButton.finalTotal())
+
+    })
+    it("should show warning colour code when it reaches R30", function(){
+        var radioButton = radioBill();
+        radioButton.callIncrement();
+        radioButton.callIncrement();
+        radioButton.callIncrement();
+        radioButton.callIncrement();
+        radioButton.callIncrement();
+        radioButton.callIncrement();
+        radioButton.callIncrement();
+        radioButton.callIncrement();
+        radioButton.callIncrement();
+        radioButton.callIncrement();
+        radioButton.callIncrement();
+        radioButton.callIncrement();
+        radioButton.callIncrement();
+        radioButton.callIncrement();
+        radioButton.callIncrement();
+        radioButton.callIncrement();
+
+        assert.equal("warning", radioButton.finalTotal())
+
+
+
+    })
+
+
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// describe("CallTotal", function () {
+
+//     it("should be able to set call total", function () {
+//         let settingsTest = RadioBill();
+
+//         settingsTest.setCallTotal("call");
+//         assert.equal(2.75, settingsTest.getCallTotal());
+
+//     });
+
+//     it("should be able to set 2 calls total", function () {
+//         let settingsTest = RadioBill();
+
+//         settingsTest.setCallTotal("call");
+//         settingsTest.setCallTotal("call");
+//         assert.equal(5.50, settingsTest.getCallTotal());
+
+//     });
+
+
+// });
+
+// describe("SMS Total", function () {
+
+//     it("should be able to calculate smsTotal", function () {
+//         let settingsTest = RadioBill();
+
+//         settingsTest.setCallTotal("sms");
+//         assert.equal(0.75, settingsTest.getSMSTotal());
+
+
+//     }); 
 
     
 
-});
+// });
 
-describe("Total", function () {
+// describe("Total", function () {
 
-    it("should be able to calculate Total", function () {
-        let settingsTest = RadioBill();
+//     it("should be able to calculate Total", function () {
+//         let settingsTest = RadioBill();
 
-        settingsTest.setCallTotal("call");
-        settingsTest.setCallTotal("call");
-        settingsTest.setCallTotal("call");
-        settingsTest.setCallTotal("call");
-        assert.equal(11 ,settingsTest.getTotal());
-
-
-    });
+//         settingsTest.setCallTotal("call");
+//         settingsTest.setCallTotal("call");
+//         settingsTest.setCallTotal("call");
+//         settingsTest.setCallTotal("call");
+//         assert.equal(11 ,settingsTest.getTotal());
 
 
-});   
+//     });
+
+
+// });   
--- RadioBillFactoryFunction.js/RadioBillFactoryFunction.js
+++ RadioBillFactoryFunction.js/RadioBillFactoryFunction.js
@@ -1,87 +1,155 @@
 function RadioBill() {
-    //var theCallTotal = 0;
-    var callsTotal = 0.00;
-  var smsTotal = 0.00;
+  var callsTotal = 0;
+  var smsTotal = 0;
   var billTotal = 0;
-  
-    function setCallTotal(billString) {
-      var billItems = billString.split(",");
-      //var billTotal = 0;
-      for (var i = 0; i < billItems.length; i++) {
-          var billItem = billItems[i].trim();
-          if (billItem === "call") {
-              billTotal += 2.75;
-              callsTotal += 2.75;
-          }
-          else if (billItem === "sms") {
-              billTotal += 0.75;
-              smsTotal += 0.75;
-          }
-      }
-    }
-    function getCallTotal(){
-       
-      return callsTotal
-      
-    } 
-  
-    // function setSMSTotal(billString) {
-    //   var billItems = billString.split(",");
-    //   //var billTotal = 0;
-    //   for (var i = 0; i < billItems.length; i++) {
-    //       var billItem = billItems[i].trim();
-    //       if (billItem === "call") {
-    //           billTotal += 0.75;
-    //           SMSTotal += 0.75;
-    //       }
-    //       else if (billItem === "sms") {
-    //           billTotal += 2.75;
-    //           CallTotal += 2.75;
-    //       }
-    //   }
-    // }
-    function getSMSTotal(){
-       
-      return smsTotal
-      
-    } 
-  
-    
-  
-    function getTotal() { 
-       
-      return billTotal;
-  
-    }
-  
-    function getWarningLevel(){
-      if (getTotal()>= 30) {
-        return "warning"
-      }
-  
-    }
-    function getDangerLevel(){
-      if (getTotal()>= 50) {
-        return "danger"
-      }
-  
-    }
-    return {
-  
-      setCallTotal,
-      getCallTotal,
-      getTotal,
-      getSMSTotal,
-      getWarningLevel,
-      getDangerLevel
-  
-  
-  
-    }
-  
+
+  function callIncrement() {
+    callsTotal += 2.75;
+
   }
-  
-  
-  
-  
-  
+  function callMade() {
+    return callsTotal.toFixed(2);
+
+
+  }
+  function smsIncrement() {
+    smsTotal += 0.75;
+
+
+  }
+  function smsMade() {
+    return smsTotal.toFixed(2);
+
+  }
+  function finalTotal() {
+    return (callsTotal + smsTotal).toFixed(2);
+
+  }
+  function warningLevel() {
+    if (finalTotal() >= 30) {
+      return 'warning'
+    }
+
+  }
+  function dangerLevel() {
+    if (finalTotal() >= 50) {
+      return 'danger'
+    }
+
+  }
+  return {
+   callIncrement,
+   callMade,
+   smsIncrement,
+   smsMade,
+   finalTotal,
+   warningLevel,
+   dangerLevel
+
+
+
+  }
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function RadioBill() {
+//     //var theCallTotal = 0;
+//     var callsTotal = 0.00;
+//   var smsTotal = 0.00;
+//   var billTotal = 0;
+
+//     function setCallTotal(billString) {
+//       var billItems = billString.split(",");
+//       //var billTotal = 0;
+//       for (var i = 0; i < billItems.length; i++) {
+//           var billItem = billItems[i].trim();
+//           if (billItem === "call") {
+//               billTotal += 2.75;
+//               callsTotal += 2.75;
+//           }
+//           else if (billItem === "sms") {
+//               billTotal += 0.75;
+//               smsTotal += 0.75;
+//           }
+//       }
+//     }
+//     function getCallTotal(){
+
+//       return callsTotal
+
+//     } 
+
+//     // function setSMSTotal(billString) {
+//     //   var billItems = billString.split(",");
+//     //   //var billTotal = 0;
+//     //   for (var i = 0; i < billItems.length; i++) {
+//     //       var billItem = billItems[i].trim();
+//     //       if (billItem === "call") {
+//     //           billTotal += 0.75;
+//     //           SMSTotal += 0.75;
+//     //       }
+//     //       else if (billItem === "sms") {
+//     //           billTotal += 2.75;
+//     //           CallTotal += 2.75;
+//     //       }
+//     //   }
+//     // }
+//     function getSMSTotal(){
+
+//       return smsTotal
+
+//     } 
+
+
+
+//     function getTotal() { 
+
+//       return billTotal;
+
+//     }
+
+//     function getWarningLevel(){
+//       if (getTotal()>= 30) {
+//         return "warning"
+//       }
+
+//     }
+//     function getDangerLevel(){
+//       if (getTotal()>= 50) {
+//         return "danger"
+//       }
+
+//     }
+//     return {
+
+//       setCallTotal,
+//       getCallTotal,
+//       getTotal,
+//       getSMSTotal,
+//       getWarningLevel,
+//       getDangerLevel
+
+
+
+//     }
+
+//   }
+
+
+
+
